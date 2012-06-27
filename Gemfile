f3f3e8b786ce61214fa16d67a3e44e7aad61dfc4
Uri Naor <u_naor@yahoo.com>
2012-06-26 23:29:20 -0400

    inicial version

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.6'
+gem 'sqlite3'
+gem 'execjs'
+gem 'therubyracer'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+gem 'rspec-rails','2.10.1'
+end
+group :test do
+gem 'rspec-rails','2.10.1'
+gem 'database_cleaner'
+gem 'factory_girl_rails'
+gem 'email_spec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '339b96a14f86108620714f3b8a3b8d5f21e394b98719cbbd4cd7252a53a936b2a2c1f7c1397612d19f81f3e2616b39c774aced5ba5e8eb8cc46d95aa48c60492'
